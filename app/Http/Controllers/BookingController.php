54b09184452c00ecb3892d9c852d0c4b5e9f4c32
moShok7 <stohirov46@gmail.com>
2025-12-29 19:20:37 +0300

    ог

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -13,22 +13,22 @@ class BookingController extends Controller
 {
    public function store(Request $request)
 {
-    // 1. Валидация данных из формы
+    
     $data = $request->validate([
         'room_id'     => 'required|exists:rooms,id',
         'started_at'  => 'required|date|after_or_equal:today',
         'finished_at' => 'required|date|after:started_at',
     ]);
 
-    // 2. Работа с датами
+    
     $start = Carbon::parse($data['started_at']);
     $end   = Carbon::parse($data['finished_at']);
     $days  = $start->diffInDays($end);
 
-    // 3. Получаем комнату
+
     $room = Room::findOrFail($data['room_id']);
 
-    // 4. Проверяем занятость номера
+   
     $isBooked = Booking::where('room_id', $room->id)
         ->where('started_at', '<', $end)
         ->where('finished_at', '>', $start)
@@ -38,19 +38,19 @@ class BookingController extends Controller
         return back()->withErrors(['room' => 'Этот номер уже занят на выбранные даты']);
     }
 
-    // 5. Создаём бронирование
+   
     $booking = Booking::create([
         'room_id'     => $room->id,
         'user_id'     => Auth::id(),
         'started_at'  => $start,
         'finished_at' => $end,
         'days'        => $days,
-        'price'       => $room->price * $days, // или $room->price_per_night
+        'price'       => $room->price * $days, 
     ]);
 
 
      Mail::to($booking->user->email)->send(new BookingConfirmed($booking));
-    // 6. Редирект на страницу бронирования с флеш-сообщением
+  
     return redirect()->route('bookings.show', $booking)
                      ->with('success', 'Бронирование успешно создано!');
 }
